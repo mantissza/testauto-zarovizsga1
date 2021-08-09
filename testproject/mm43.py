5683333cfad61d05fefdb0420841de660e7cae03
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-08-09 19:47:09 +0200

    4 Feladat: Email mező

---
+++ testproject/mm43.py
@@ -0,0 +1,31 @@
+#  4 Feladat: Email mező
+
+from selenium import webdriver
+#  from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+import time
+import random
+import string
+import sys
+
+driver = webdriver.Chrome()
+#  driver = webdriver.Chrome(ChromeDriverManager().install())
+URL = "https://black-moss-0a0440e03.azurestaticapps.net/mm43.html"
+
+driver.get(URL)
+
+####################################################
+#                PYTHON FUNCTIONS
+####################################################
+
+
+####################################################
+#                    WORKBENCH
+####################################################
+
+
+####################################################
+#                  TEST CASES
+####################################################
+
+
